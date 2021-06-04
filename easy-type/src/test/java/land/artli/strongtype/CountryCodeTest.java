de4c9b748566cc7df5faec0b255412db2d863d79
evantoli <evantoliopoulos@softwarekitchen.com.au>
2021-06-04 12:39:30 +1000

    Reorganised code (#4)
    * Reorganised code
    
    * Added dash handling
    * Turned it into a multi-module project with an eye to generating some Java enums for the unicode categories, blocks, and scripts.
    * Optimized imports
    * Replaced large unicode all XML with unicode groups XML
    
    * Restored readme to root location
    
    Co-authored-by: Evan Toliopoulos <van@artli.land>

---
+++ easy-type/src/test/java/land/artli/strongtype/CountryCodeTest.java
@@ -0,0 +1,20 @@
+package land.artli.strongtype;
+
+import java.text.ParseException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CountryCodeTest {
+
+  @ParameterizedTest
+  @CsvSource(value = {
+      "AU|AU",
+      "uS|US",
+      "Nz|NZ",
+      "de|DE",
+  }, delimiter = '|')
+  void should_parse_valid_values(final String value, final String expected) throws ParseException {
+    Assertions.assertThatObject(CountryCode.of(value)).hasToString(expected);
+  }
+}
