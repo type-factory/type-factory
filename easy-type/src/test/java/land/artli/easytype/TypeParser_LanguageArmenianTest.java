7641d03920568c57e47acbf0e3de7bbd2854e096
Evan Toliopoulos <van@artli.land>
2021-12-17 19:53:24 +1100

    * Added some Language subsets

--- easy-type/src/test/java/land/artli/easytype/TypeParser_LanguageArmenianTest.java
+++ easy-type/src/test/java/land/artli/easytype/TypeParser_LanguageArmenianTest.java
@@ -5,21 +5,21 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class TypeParser_LanguageArabicTest {
+public class TypeParser_LanguageArmenianTest {
 
   @ParameterizedTest
   @ValueSource(strings = {
-      "جاموس", // Arabic 'Buffalo'
-      "حصان", // Arabic 'Horse'
-      "قنديل البحر", // Arabic 'Jellyfish'
+      "ռնգեղջյուր", // Armenian 'rhinoceros'
+      "թիթեռ", // Armenian 'butterfly'
+      "կապիկ", // Armenian 'monkey'
   })
-  void should_parse_accepting_only_arabic_letters(final String value) {
+  void should_parse_accepting_only_armenian_letters(final String value) {
 
     final TypeParser typeParser =
         TypeParser.builder(SomeType.class)
-            .errorMessage("Must be made up of Arabic letters only.")
+            .errorMessage("Must be made up of Armenian letters only.")
             .toCharacterNormalizationFormNFC()
-            .acceptLanguage(Language.LETTERS_ARABIC_AR)
+            .acceptLanguage(Language.LETTERS_ARMENIAN_HY)
             .normalizeWhitespace()
             .build();
     Assertions.assertThat(typeParser.parse(value)).hasToString(value);
