49daeb9293fe07c750dd060f9a6bf03cb663ab05
Evan Toliopoulos <8422620+evantoli@users.noreply.github.com>
2022-11-07 19:14:24 +1100

    Add license headers to source files (#84)
    * Added license header to each source file
    
    * Added license header to each source file

--- type-factory-core/src/main/java/org/typefactory/impl/CodePointSequenceToCodePointSequenceConverter.java
+++ type-factory-core/src/main/java/org/typefactory/impl/CodePointSequenceToCodePointSequenceConverter.java
@@ -1,6 +1,20 @@
+/*
+   Copyright 2021-2022 Evan Toliopoulos (typefactory.org)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package org.typefactory.impl;
 
-import static org.typefactory.impl.Constants.EMPTY_INT_ARRAY;
 import static org.typefactory.impl.Constants.LINE_SEPARATOR;
 
 import java.util.ArrayDeque;
@@ -14,11 +28,6 @@ class CodePointSequenceToCodePointSequenceConverter implements Converter {
   private final RootTreeNode rootTreeNode;
 
   /**
-   * Hash-map of Character categories to code-point arrays:
-   */
-  private final PrimitiveHashMapOfIntKeyToIntArrayValue categoryToCodePointSequence;
-
-  /**
    * The maximum number of code-points that a single code-point could be converted to.
    */
   private final int maxConvertedLength;
@@ -27,7 +36,6 @@ class CodePointSequenceToCodePointSequenceConverter implements Converter {
       final RootTreeNode rootTreeNode,
       final PrimitiveHashMapOfIntKeyToIntArrayValue categoryToCodePointSequence) {
     this.rootTreeNode = rootTreeNode;
-    this.categoryToCodePointSequence = categoryToCodePointSequence;
     int max = 0;
     if (rootTreeNode != null && !rootTreeNode.isEmpty()) {
       max = Math.max(max, rootTreeNode.getMaxToSequenceLength());
