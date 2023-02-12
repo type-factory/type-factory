c1921045baca94ebdf13e3770a87fc38232642cf
Evan Toliopoulos <8422620+evantoli@users.noreply.github.com>
2023-02-12 12:36:24 +1100

    more unit tests

--- type-factory-core/src/test/java/org/typefactory/impl/ConverterTest.java
+++ type-factory-core/src/test/java/org/typefactory/impl/ConverterTest.java
@@ -294,7 +294,53 @@ class ConverterTest {
       abc-ΔΣΩ | [1,2,Σ] | z | abc-ΔzΩ
       abc-ΔΣ2 | [1,2,Σ] | z | abc-Δzz
       """, delimiter = '|')
-  void addCodePointConversions_fromSubsetToCharSequenceReturnsAsExpected(
+  void addCodePointConversions_fromSubsetToCharReturnsAsExpected(
+      final String input,
+      @ConvertWith(CodePointArrayConverter.class) final int[] fromCodePoints,
+      final char toChar,
+      final String expected) {
+
+    final var subset = Subset.builder()
+        .includeCodePoints(fromCodePoints)
+        .build();
+
+    final var converter = new ConverterBuilder()
+        .addCodePointConversions(subset, toChar)
+        .build();
+
+    final String actual = convert(input, converter);
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @ParameterizedTest
+  @CsvSource(textBlock = """
+      1       | [a,2,Σ] | z | 1
+      11      | [a,2,Σ] | z | 11
+      12      | [a,2,Σ] | z | 1z
+      13      | [a,2,Σ] | z | 13
+      a       | [a,2,Σ] | z | z
+      aa      | [a,2,Σ] | z | zz
+      abcdef1 | [a,2,Σ] | z | zbcdef1
+      abcdef2 | [a,2,Σ] | z | zbcdefz
+      abc-aba | [a,2,Σ] | z | zbc-zbz
+      abc-123 | [a,2,Σ] | z | zbc-1z3
+      abc-ΔΣΩ | [a,2,Σ] | z | zbc-ΔzΩ
+      abc-ΔΣ2 | [a,2,Σ] | z | zbc-Δzz
+      1       | [1,2,Σ] | z | z
+      11      | [1,2,Σ] | z | zz
+      12      | [1,2,Σ] | z | zz
+      13      | [1,2,Σ] | z | z3
+      a       | [1,2,Σ] | z | a
+      aa      | [1,2,Σ] | z | aa
+      abcdef1 | [1,2,Σ] | z | abcdefz
+      abcdef2 | [1,2,Σ] | z | abcdefz
+      abc-aba | [1,2,Σ] | z | abc-aba
+      abc-123 | [1,2,Σ] | z | abc-zz3
+      abc-ΔΣΩ | [1,2,Σ] | z | abc-ΔzΩ
+      abc-ΔΣ2 | [1,2,Σ] | z | abc-Δzz
+      """, delimiter = '|')
+  void addCodePointConversions_fromSubsetToCodePointReturnsAsExpected(
       final String input,
       @ConvertWith(CodePointArrayConverter.class) final int[] fromCodePoints,
       @ConvertWith(CodePointConverter.class) final int toCodePoint,
@@ -306,6 +352,52 @@ class ConverterTest {
 
     final var converter = new ConverterBuilder()
         .addCodePointConversions(subset, toCodePoint)
+        .build();
+
+    final String actual = convert(input, converter);
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @ParameterizedTest
+  @CsvSource(textBlock = """
+      1       | [a,2,Σ] | zΨ | 1
+      11      | [a,2,Σ] | zΨ | 11
+      12      | [a,2,Σ] | zΨ | 1zΨ
+      13      | [a,2,Σ] | zΨ | 13
+      a       | [a,2,Σ] | zΨ | zΨ
+      aa      | [a,2,Σ] | zΨ | zΨzΨ
+      abcdef1 | [a,2,Σ] | zΨ | zΨbcdef1
+      abcdef2 | [a,2,Σ] | zΨ | zΨbcdefzΨ
+      abc-aba | [a,2,Σ] | zΨ | zΨbc-zΨbzΨ
+      abc-123 | [a,2,Σ] | zΨ | zΨbc-1zΨ3
+      abc-ΔΣΩ | [a,2,Σ] | zΨ | zΨbc-ΔzΨΩ
+      abc-ΔΣ2 | [a,2,Σ] | zΨ | zΨbc-ΔzΨzΨ
+      1       | [1,2,Σ] | zΨ | zΨ
+      11      | [1,2,Σ] | zΨ | zΨzΨ
+      12      | [1,2,Σ] | zΨ | zΨzΨ
+      13      | [1,2,Σ] | zΨ | zΨ3
+      a       | [1,2,Σ] | zΨ | a
+      aa      | [1,2,Σ] | zΨ | aa
+      abcdef1 | [1,2,Σ] | zΨ | abcdefzΨ
+      abcdef2 | [1,2,Σ] | zΨ | abcdefzΨ
+      abc-aba | [1,2,Σ] | zΨ | abc-aba
+      abc-123 | [1,2,Σ] | zΨ | abc-zΨzΨ3
+      abc-ΔΣΩ | [1,2,Σ] | zΨ | abc-ΔzΨΩ
+      abc-ΔΣ2 | [1,2,Σ] | zΨ | abc-ΔzΨzΨ
+      """, delimiter = '|')
+  void addCodePointConversions_fromSubsetToCharSequenceReturnsAsExpected(
+      final String input,
+      @ConvertWith(CodePointArrayConverter.class) final int[] fromCodePoints,
+      final String toCharSequence,
+      final String expected) {
+
+    final var subset = Subset.builder()
+        .includeCodePoints(fromCodePoints)
+        .build();
+
+    final var converter = new ConverterBuilder()
+        .addCodePointConversions(subset, toCharSequence)
         .build();
 
     final String actual = convert(input, converter);
