2ff167498d62aef707c2e6b4b005a5f2354d31fb
Evan Toliopoulos <8422620+evantoli@users.noreply.github.com>
2024-07-13 16:30:38 +1000

    Issue #201 Added unit tests

--- type-factory-core/src/test/java/org/typefactory/impl/LongTypeParserBuilderImplTest.java
+++ type-factory-core/src/test/java/org/typefactory/impl/LongTypeParserBuilderImplTest.java
@@ -1,7 +1,23 @@
+/*
+   Copyright 2021-2022 Evan Toliopoulos (typefactory.org)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package org.typefactory.impl;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -36,5 +52,20 @@ class LongTypeParserBuilderImplTest {
         .isThrownBy(builder::build)
         .withMessageStartingWith(expectedMessage);
   }
+
+  @Test
+  void allowCustomBaseNumbers_nullBaseCharactersThrowsException() {
+    assertThatExceptionOfType(TypeParserBuilderException.class)
+        .isThrownBy(() -> LongTypeParser.builder().allowCustomBaseNumbers((int[]) null))
+        .withMessageStartingWith("An invalid numeric-base has been configured for the numeric type parser – provide at least two characters for a radix of two or greater in your parser.");
+  }
+
+  @Test
+  void allowCustomBaseNumbers_singleCharacterArrayForBaseCharactersThrowsException() {
+    assertThatExceptionOfType(TypeParserBuilderException.class)
+        .isThrownBy(() -> LongTypeParser.builder().allowCustomBaseNumbers(new int[]{'0'}))
+        .withMessageStartingWith("An invalid numeric-base has been configured for the numeric type parser – provide at least two characters for a radix of two or greater in your parser.");
+  }
+
 
 }
