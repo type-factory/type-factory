cf495a9cdfbd10f20681dae8e2232d646b2c8006
Evan Toliopoulos <8422620+evantoli@users.noreply.github.com>
2025-06-11 17:14:50 +1000

    feature(289): Added unit tests

--- type-factory-core/src/test/java/org/typefactory/impl/TypeParser_RemoveInvalidCharactersTest.java
+++ type-factory-core/src/test/java/org/typefactory/impl/TypeParser_RemoveInvalidCharactersTest.java
@@ -46,15 +46,15 @@ class TypeParser_RemoveInvalidCharactersTest {
   @ParameterizedTest(name = "[{index}] {arguments}")
   @CsvSource(textBlock = """
       VALUE                                             | IS_VALID_VALUE | INVALID_CODEPOINTS | EXPECTED_VALUE
-      abcdefg                                           | true           |                    | abcdefg
-      abc1234                                           | true           |                    | abc1234
-      abc-123                                           | false          | -                  | abc123
-      123_abc                                           | false          | _                  | 123abc
-      $bcdefg                                           | false          | $                  | bcdefg
-      abcdef$                                           | false          | $                  | abcdef
-      abcd$fg                                           | false          | $                  | abcdfg
-      abcd$#%                                           | false          | $#%                | abcd
-      abc defg                                          | false          | ' '                | abcdefg
+      abcdefg                                           | true           |                    | aBcdefg
+      abc1234                                           | true           |                    | aBc1234
+      abc-123                                           | false          | -                  | aBc123
+      123_abc                                           | false          | _                  | 123aBc
+      $bcdefg                                           | false          | $                  | Bcdefg
+      abcdef$                                           | false          | $                  | aBcdef
+      abcd$fg                                           | false          | $                  | aBcdfg
+      abcd$#%                                           | false          | $#%                | aBcd
+      abc defg                                          | false          | ' '                | aBcdefg
       $(".classname").each(function(){alert("Boo!");}); | false          | $()!".{};          | classnameeachfunctionalertBoo
       """, delimiter = '|', useHeadersInDisplayName = true)
   void parseToStringRemovingInvalidCharacters_returnsAsExpectedWithCharacterConversion(
@@ -64,6 +64,7 @@ class TypeParser_RemoveInvalidCharactersTest {
       final String expectedValue) {
 
     final var typeParser = TypeParser.builder()
+        .convertChar('b', 'B')
         .acceptAllUnicodeLetters()
         .acceptAllUnicodeDecimalDigits()
         .build();
