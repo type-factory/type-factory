486d285592cdbf106b60ca8a1e03c39371cd557f
Evan Toliopoulos <8422620+evantoli@users.noreply.github.com>
2026-02-22 16:47:11 +1100

    Added tests

--- type-factory-test-assertj/src/main/java/org/typefactory/assertions/AssertionUtils.java
+++ type-factory-test-assertj/src/main/java/org/typefactory/assertions/AssertionUtils.java
@@ -1,7 +1,5 @@
 package org.typefactory.assertions;
 
-import static org.typefactory.Category.codePointIsInOneOfTheCategories;
-
 import org.typefactory.Category;
 import org.typefactory.CharSequenceUtils;
 
@@ -10,14 +8,6 @@ final class AssertionUtils {
   private AssertionUtils() {
     // don't instantiate me
   }
-
-  private static final long SPACE_CONTROL_AND_FORMAT_CATEGORY_BIT_FLAGS =
-      Category.getCategoryBitFlags(
-          Category.CONTROL,
-          Category.FORMAT,
-          Category.SPACE_SEPARATOR,
-          Category.LINE_SEPARATOR,
-          Category.PARAGRAPH_SEPARATOR);
 
   static final String NULL_VALUE = "null";
   static final String EMPTY_VALUE = "\"\" (empty)";
@@ -67,61 +57,4 @@ final class AssertionUtils {
     return !isEmpty(array);
   }
 
-  static String codePointToString(final int codePoint) {
-    if (Character.isDefined(codePoint)) {
-      if (Category.codePointIsInOneOfTheCategories(codePoint, SPACE_CONTROL_AND_FORMAT_CATEGORY_BIT_FLAGS)) {
-        return codePoint > 0xFFFF
-            ? String.format("U+%06X %s", codePoint, Character.getName(codePoint))
-            : String.format("U+%04X %s", (short) codePoint, Character.getName(codePoint));
-      }
-      if (Character.isHighSurrogate((char) codePoint)) {
-        return String.format("U+%04X HIGH SURROGATE", (short) codePoint);
-      }
-      if (Character.isLowSurrogate((char) codePoint)) {
-        return String.format("U+%04X LOW SURROGATE", (short) codePoint);
-      }
-      return codePoint > 0xFFFF
-          ? String.format("%c U+%06X %s", codePoint, codePoint, Character.getName(codePoint))
-          : String.format("%c U+%04X %s", codePoint, (short) codePoint, Character.getName(codePoint));
-    }
-
-    if (!Character.isValidCodePoint(codePoint)) {
-      return codePoint > 0xFFFFFFL
-          ? String.format("U+%08X NOT A UNICODE CHARACTER", codePoint)
-          : String.format("U+%06X NOT A UNICODE CHARACTER", codePoint);
-    }
-
-    return codePoint > 0xFFFF
-        ? String.format("U+%06X UNASSIGNED UNICODE CHARACTER", codePoint)
-        : String.format("U+%04X UNASSIGNED UNICODE CHARACTER", codePoint);
-  }
-
-  static String unicodeHexCode(final int codePoint) {
-    if (Character.isDefined(codePoint)) {
-      if (codePointIsInOneOfTheCategories(codePoint, SPACE_CONTROL_AND_FORMAT_CATEGORY_BIT_FLAGS)) {
-        return codePoint > 0xFFFF
-            ? String.format("U+%06X %s", codePoint, Character.getName(codePoint))
-            : String.format("U+%04X %s", (short) codePoint, Character.getName(codePoint));
-      }
-      if (Character.isHighSurrogate((char) codePoint)) {
-        return String.format("U+%04X HIGH SURROGATE", (short) codePoint);
-      }
-      if (Character.isLowSurrogate((char) codePoint)) {
-        return String.format("U+%04X LOW SURROGATE", (short) codePoint);
-      }
-      return codePoint > 0xFFFF
-          ? String.format("%c U+%06X %s", codePoint, codePoint, Character.getName(codePoint))
-          : String.format("%c U+%04X %s", codePoint, (short) codePoint, Character.getName(codePoint));
-    }
-
-    if (!Character.isValidCodePoint(codePoint)) {
-      return codePoint > 0xFFFFFFL
-          ? String.format("U+%08X NOT A UNICODE CHARACTER", codePoint)
-          : String.format("U+%06X NOT A UNICODE CHARACTER", codePoint);
-    }
-
-    return codePoint > 0xFFFF
-        ? String.format("U+%06X UNASSIGNED UNICODE CHARACTER", codePoint)
-        : String.format("U+%04X UNASSIGNED UNICODE CHARACTER", codePoint);
-  }
 }
