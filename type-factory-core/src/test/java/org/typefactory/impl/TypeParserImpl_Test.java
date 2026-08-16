d88d4324455ca670eedde877f76609b577d22b42
Evan Toliopoulos <8422620+evantoli@users.noreply.github.com>
2026-08-16 19:01:41 +1000

    chore: Update license information
    Signed-off-by: Evan Toliopoulos <8422620+evantoli@users.noreply.github.com>

--- type-factory-core/src/test/java/org/typefactory/impl/TypeParserImpl_Test.java
+++ type-factory-core/src/test/java/org/typefactory/impl/TypeParserImpl_Test.java
@@ -1,24 +1,23 @@
 /*
-   Copyright 2021-2022 Evan Toliopoulos (typefactory.org)
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+ * Copyright © 2021-2026 Evan Toliopoulos (typefactory.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.typefactory.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatObject;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -26,8 +25,6 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.typefactory.ShortType;
 import org.typefactory.TypeParser;
-import org.typefactory.testutils.CodePointArrayConverter;
-import org.typefactory.testutils.CodePointConverter;
 import org.typefactory.testutils.CodePointSequenceConverter;
 
 class TypeParserImpl_Test {
--- type-factory-examples/src/test/java/org/typefactory/recordtypes/CountryCodeTest.java
+++ type-factory-examples/src/test/java/org/typefactory/recordtypes/CountryCodeTest.java
@@ -1,25 +1,24 @@
 /*
-   Copyright 2021-2022 Evan Toliopoulos (typefactory.org)
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+ * Copyright © 2021-2026 Evan Toliopoulos (typefactory.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.typefactory.recordtypes;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatObject;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
